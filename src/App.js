795861d485bb81cdcc76108a5abdd5c711e24389
divyya1104 <divyyasrees@gmail.com>
2024-01-26 15:27:54 +0530

    bab

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import {BrowserRouter , Routes , Route} from "react-router-dom"
+ import {BrowserRouter , Routes , Route} from "react-router-dom"
 import Layout from "./Layout"
 import Home from "./Pages/Home"
 import About from "./Pages/About"
 import Contact from "./Pages/Contact"
 import Service from "./Pages/Service"
+import Counter from "./Counter.js";
+import Effect from "./Effect.js"
+import Reducer from "./Reducer.js"
+
 export default function App(){
   return(
-    <BrowserRouter>
-      <Routes>
-         <Route path="/" element={<Layout/>}>
-         <Route index element={<Home/>}/>
-         <Route path="about" element={<About/>}/>
-         <Route path="contact" element={<Contact/>}/>
-         <Route path="service" element={<Service/>}/>
-          </Route>
-      </Routes>
-    </BrowserRouter>
+//     <BrowserRouter>
+//       <Routes>
+//          <Route path="/" element={<Layout/>}>
+//          <Route index element={<Home/>}/>
+//          <Route path="about" element={<About/>}/>
+//          <Route path="contact" element={<Contact/>}/>
+//          <Route path="service" element={<Service/>}/>
+//           </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+
+    <div>
+    <Counter/>
+    <Effect/>
+    <Reducer/>
+    </div>
+
   )
 }
-
